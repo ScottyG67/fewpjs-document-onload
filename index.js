859095291f85ec130f5c1a3d5addabd7f704f0ea
github username <scott.gloyna01@gmail.com>
2021-02-23 15:32:29 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    updateText();
+  }); 
+
+function updateText(){
+    let newText = document.getElementById("text");
+    newText.innerHTML = "This is really cool!";
+};
